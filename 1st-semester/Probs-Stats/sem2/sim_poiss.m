fa6010f7f78719c101c6383872cd6d4c4f22d84e
Split(',').rstrip() <marccasiannicolae@gmail.com>
2018-10-22 19:39:13 +0300

    done lab2

--- 1st-semester/Probs-Stats/sem2/sim_poiss.m
+++ 1st-semester/Probs-Stats/sem2/sim_poiss.m
@@ -7,25 +7,22 @@ N = input('number of simulations='); % par
 
 for j=1:N
     U = rand;
-    F = exp(-l);
-    while U >= F
-        i+=1
-    for j=1:n
-        Y(j) = 0;
-        while rand >= p % count nr of failures
-            Y(j) = Y(j) + 1;
-        end;
+    i=0;
+    F(j) = exp(-l);
+    while U >= F(j)
+        i = i + 1;
+        F(j) = F(j)+exp(-l)*l^i/gamma(i+1);
     end;
-    X(i) = sum(Y);
+    X(j)=i;
 end;
-% compare it to the Geo(n, p) distr graphically
+% compare it to the Poiss(l) distr graphically
 
 UX = unique(X);
 nX = hist(X, length(UX));
 rel_freq = nX/N;
 
 k = 0:50; % NBin distr as a infinite nr of values
-pk = nbinpdf(k,n,p); % true NBin(n, p)
+pk = poisspdf(k,l); % true Poiss(n, p)
 clf
 plot(k, pk, '*', UX, rel_freq, 'ro')
-legend('NBin distr', 'Simulation')
+legend('Poiss distr', 'Simulation')
