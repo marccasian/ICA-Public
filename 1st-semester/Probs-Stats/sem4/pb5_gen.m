3b0130fdee315dac5069f0b2afcca765fc4f56cd
Split(',').rstrip() <marccasiannicolae@gmail.com>
2018-11-19 20:06:08 +0200

    Add files via upload
    add sem 4

--- 1st-semester/Probs-Stats/sem4/pb5_gen.m
+++ 1st-semester/Probs-Stats/sem4/pb5_gen.m
@@ -2,6 +2,8 @@
 % b
 Nm = input('Length of samplepath =');
 N = input('Number of simulaton =');
+inundatii = 0;
+sunnyd = 0;
 for j = 1:N
     P0 = [0.2 0.8]; % initial distr of sunny/rainy
     P = [0.7 0.3; 0.4 0.6]; % trans prob matrix
@@ -21,7 +23,26 @@ for j = 1:N
     for i = 2:length(ichange)
         longstr(i) = ichange(i) - ichange(i-1);
     end
-    fprintf('\n streaks of sunny/rainy:\n')
-    fprintf(num2str(longstr))
-    X
+    %fprintf('\n streaks of sunny/rainy:\n')
+    %fprintf(num2str(longstr))
+    %X
+    if X(1) == 1
+        sunny  = longstr(1:2:end);
+        rainy  = longstr(2:2:end);
+    else
+        rainy  = longstr(1:2:end);
+        sunny  = longstr(2:1:end);
+    end
+    maxs(j) = max(sunny);
+    maxr(j) = max(rainy);
+%    if max(rainy) >= 7
+%        inundatii = inundatii +1;
+%    end
+%    if max(sunny) >= 7
+%        sunnyd = sunnyd +1;
+%    end
 end
+fprintf('\n Prob of water shortage is %1.4f\n', mean(maxs>=7));
+fprintf('\n Prob of flooding is %1.4f\n', mean(maxr>=7));
+%probinundatie = inundatii / N
+%ProbSeceta = sunnyd / N
